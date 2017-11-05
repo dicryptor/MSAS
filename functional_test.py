fb683f5c910a4269ace691fd582372c667875b03
Mohamed Mubaraq <dicryptor@gmail.com>
2017-11-05 03:01:08 +0000

    clean up unit tests

--- functional_test.py
+++ functional_test.py
@@ -6,38 +6,40 @@ class BlindspotTest(unittest.TestCase):
     unit test for blindspot detection
     '''
 
-    #sensors = []
-
-
     def setUp(self):
-        self.left_front = blindspot.USensor(0)
-        self.right_front = blindspot.USensor(1)
-        #self.sensors.append(self.left_front)
-        #self.sensors.append(self.right_front)
+        self.leftFront = blindspot.USensor(0)
+        self.rightFront = blindspot.USensor(1)
+        self.leftBack = blindspot.USensor(2)
+        self.rightBack = blindspot.USensor(3)
 
-    def test_leftFront_gt_0(self):
-        #self.vals = list(map(lambda x:x.get_value(), self.sensors))
-        #print(self.vals)
-        self.assertGreater(self.left_front.get_value(), 0, "Sensor reading greater than 0") 
+    ### Check for sensor reading, GT 10(noise readings) and LT 1600(max value at current gain)
+    def test_leftFront_gt_10(self):
+        self.assertGreater(self.leftFront.get_value(), 10, "Sensor reading greater than 0") 
 
-    def test_rightFront_gt_0(self):
-        #self.vals = list(map(lambda x:x.get_value(), self.sensors))
-        #print(self.vals)
-        self.assertGreater(self.right_front.get_value(), 0, "Sensor reading greater than 0") 
+    def test_rightFront_gt_10(self):
+        self.assertGreater(self.rightFront.get_value(), 10, "Sensor reading greater than 0") 
+
+    def test_leftBack_gt_10(self):
+        val = self.leftBack.get_value()
+        self.assertGreater(val, 10, "Left back sensor: {}".format(val)) 
+
+    def test_rightBack_gt_10(self):
+        val = self.rightBack.get_value()
+        self.assertGreater(val, 10, "Right back sensor: {}".format(val)) 
 
     def test_leftFront_lt_1600(self):
-        #self.vals = list(map(lambda x:x.get_value(), self.sensors))
-        #print(self.vals)
-        self.assertLess(self.left_front.get_value(), 1600) 
+        self.assertLess(self.leftFront.get_value(), 1600) 
 
     def test_rightFront_lt_1600(self):
-        #self.vals = list(map(lambda x:x.get_value(), self.sensors))
-        #print(self.vals)
-        self.assertLess(self.right_front.get_value(), 1600) 
+        self.assertLess(self.rightFront.get_value(), 1600) 
+
+    def test_leftBack_lt_1600(self):
+        self.assertLess(self.leftBack.get_value(), 1600) 
+
+    def test_rightBack_lt_1600(self):
+        self.assertLess(self.rightBack.get_value(), 1600) 
 
 
 if __name__ == "__main__":
-    unittest.main()
-
-
+    unittest.main(warnings='ignore')
 
