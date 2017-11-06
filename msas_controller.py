22fe82357bf8db4695600158a01410d2e6db573e
Mohamed Mubaraq <dicryptor@gmail.com>
2017-11-06 01:10:57 +0000

    updates controller to use led class

--- msas_controller.py
+++ msas_controller.py
@@ -1,17 +1,18 @@
 import sys
 import time
 import blindspot
-import RPi.GPIO as GPIO
+import led_notification
 
 DETECT = 100
-GPIO.setmode(GPIO.BCM)
+##GPIO.setmode(GPIO.BCM)
 RIGHT_LED = 20
 LEFT_LED = 26
-GPIO.setup([RIGHT_LED,LEFT_LED], GPIO.OUT)
+##GPIO.setup([RIGHT_LED,LEFT_LED], GPIO.OUT)
 
 left_front = blindspot.USensor(0)
 right_front = blindspot.USensor(1)
-
+leftLed = led_notification.LED(LEFT_LED)
+rightLed = led_notification.LED(RIGHT_LED)
 
 
 
@@ -22,15 +23,15 @@ try:
         print("| {:>6} | {:>6}".format(val1, val2))
 
         if left_front.get_value() < DETECT:
-            GPIO.output(LEFT_LED, GPIO.HIGH)
+            leftLed.ledOn()
         else:
-            GPIO.output(LEFT_LED, GPIO.LOW)
+            leftLed.ledOff()
 
         if right_front.get_value() < DETECT:
-            GPIO.output(RIGHT_LED, GPIO.HIGH)
+            rightLed.ledOn()
         else:
-            GPIO.output(RIGHT_LED, GPIO.LOW)
+            rightLed.ledOff()
         time.sleep(0.2)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    led_notification.cleanUp()
