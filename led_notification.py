6092f3146a1034153b1d25e6a4baa24a6519daa3
Mohamed Mubaraq <dicryptor@gmail.com>
2017-11-06 00:59:22 +0000

    added class for led notification

---
+++ led_notification.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+class LED:
+    '''
+    to manage led notification
+    '''
+
+    def __init__(self, PIN):
+        self.PIN = PIN
+        GPIO.setup(self.PIN, GPIO.OUT)
+        ## init led indication
+        GPIO.output(self.PIN, GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(self.PIN, GPIO.LOW)
+
+
+    def ledOn(self):
+        GPIO.output(self.PIN, GPIO.HIGH)
+
+
+    def ledOff(self):
+        GPIO.output(self.PIN, GPIO.LOW)
+
+
+
+
+def cleanUp():
+    GPIO.cleanup() 
