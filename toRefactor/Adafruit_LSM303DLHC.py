a5129560fb5f42d33da9ffada4fcfd3592f886d2
Mohamed Mubaraq <dicryptor@gmail.com>
2017-11-18 12:23:01 +0800

    Updated smbus import

--- toRefactor/Adafruit_LSM303DLHC.py
+++ toRefactor/Adafruit_LSM303DLHC.py
@@ -3,12 +3,11 @@
 import sys
 import os
 import time
-import smbus
+import smbus2 as smbus
 import math
 from datetime import datetime, date
 from Adafruit_I2C import Adafruit_I2C
 import unittest
-import weewee
 
 
 # ===========================================================================
