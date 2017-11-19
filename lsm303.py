2c41df03d3b4700cd1c33224313f602f09cf2446
Mohamed Mubaraq <dicryptor@gmail.com>
2017-11-20 07:31:24 +0800

    Merge branch 'master' of C:\Users\mmbabdul\Google Drive\Study Stuff\IP\MSAS\MSAS with conflicts.

--- lsm303.py
+++ lsm303.py
@@ -57,11 +57,29 @@ class LSM303(object):
         # output is padded to compatible 12-bit units.
         if hires:
             # self._accel.write8(LSM303_REGISTER_ACCEL_CTRL_REG4_A, 0b00111000)
-            self._accel.write8(LSM303_REGISTER_ACCEL_CTRL_REG4_A, 0b00011000)
+            self._accel.write8(LSM303_REGISTER_ACCEL_CTRL_REG4_A, 0b0001000)
         else:
             self._accel.write8(LSM303_REGISTER_ACCEL_CTRL_REG4_A, 0)
             # Enable the magnetometer
             # self._mag.write8(LSM303_REGISTER_MAG_MR_REG_M, 0x00)
+
+    # def setAccelerometerScale(self, scale):
+    #     "Sets the accelerometer measurement scale"
+    #     if scale == 2:
+    #         self.accelScale = 0b00
+    #         self.accelFactor = 0.001
+    #     elif scale == 4:
+    #         self.accelScale = 0b01
+    #         self.accelFactor = 0.002
+    #     elif scale == 8:
+    #         self.accelScale = 0b10
+    #         self.accelFactor = 0.004
+    #     elif scale == 16:
+    #         self.accelScale = 0b11
+    #         self.accelFactor = 0.012
+    #     else:
+    #         print("setAccelerometerScale takes values 2, 4, 8 or 16 only")
+    #     self.__setCtrlReg4A()
 
     def read(self):
         """Read the accelerometer and magnetometer value.  A tuple of tuples will
@@ -87,7 +105,7 @@ class LSM303(object):
         realAccel = [0.0, 0.0, 0.0]
         accel = self.read()
         for i in range(3):
-            realAccel[i] = round(accel[i] * 0.001, 3)
+            realAccel[i] = round(accel[i] * 0.002, 3) # scaling factor at +-4g, 0.002
         return realAccel
 
 
