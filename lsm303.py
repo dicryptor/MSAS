68f4605fadf1882b44d7ece88f5177f89908b8bf
Mohamed Mubaraq <dicryptor@gmail.com>
2017-11-19 20:57:26 +0800

    back to the original recipe

--- lsm303.py
+++ lsm303.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import struct, time
+import struct, time, math
 
 # Minimal constants carried over from Arduino library:
 LSM303_ADDRESS_ACCEL = (0x32 >> 1)  # 0011001x
