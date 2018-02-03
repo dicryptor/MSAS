05f40fb3143e214ad4783a8f2d426e8e7c8faa02
Mohamed Mubaraq <dicryptor@gmail.com>
2018-02-03 15:14:14 +0800

    GPS is in the house.

--- location_info.py
+++ location_info.py
@@ -9,7 +9,7 @@ class GPSD():
         self.all_data = None
 
     def get_data(self):
-        self.all_data = gpsd.get_data()
+        self.all_data = gpsd.get_current()
 
     def del_data(self):
         self.all_data = None
@@ -29,8 +29,8 @@ class GPSD():
     def get_track(self):
         return self.all_data.track
 
-    def get_hspeed(self):
-        return self.all_data.speed
+    def get_speed(self):
+        return self.all_data.speed()
 
 
 
@@ -41,13 +41,13 @@ if __name__ == "__main__":
     while True:
         try:
             gps.get_data()
+            if gps.get_mode() > 2:
+                print("Time: {}, Latitude: {}, Longitude: {}, Track: (), Speed: {}".format(
+                    gps.get_time(), gps.get_lat(), gps.get_lon(), gps.get_track(), gps.get_speed()))
+            time.sleep(1)
+            # gps.del_data()
         except Exception as e:
             print(e)
         except KeyboardInterrupt:
             print("User stopped the process")
-
-        if gps.get_mode() > 2:
-            print("Time: {}, Latitude: {}, Longitude: {}, Track: (), Speed: {}".format(
-                gps.get_time(), gps.get_lat(), gps.get_lon(), gps.get_track(), gps.get_hspeed()))
-        time.sleep(1)
-        gps.del_data()
+            break
