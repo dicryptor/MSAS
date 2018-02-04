cf91c9c7f01a234eadfa6e309eb486aa9875c457
Mohamed Mubaraq <dicryptor@gmail.com>
2018-02-04 09:40:23 +0800

    Debugging

--- location_info_v2.py
+++ location_info_v2.py
@@ -9,6 +9,8 @@ class AGPS():
         self.gpsd_socket.connect()
         self.gpsd_socket.watch()
         self.data_stream = agps3.DataStream()
+        self.location_info = {"time": None, "lat": None, "lon": None, "speed": None, "track": None}
+        self.timestamp = None
 
 
     def shutdown(self):
@@ -20,17 +22,19 @@ class AGPS():
         for new_data in self.gpsd_socket:
             if new_data:
                 self.data_stream.unpack(new_data)
-                print(self.data_stream.lat)
-                print(self.data_stream.lon)
+                if self.timestamp == None: self.timestamp = self.data_stream.time
+                if self.data_stream.lat != "n/a" and self.data_stream.lon != "n/a":
+                    self.location_info["lat"] = self.data_stream.lat
+                    self.location_info["lon"] = self.data_stream.lon
+                    self.location_info["time"] = self.data_stream.time
+                if self.data_stream.speed != "n/a": self.location_info["speed"] = self.data_stream.speed
+                if self.data_stream.track != "n/a": self.location_info["track"] = self.data_stream.track
+                return
             else:
                 time.sleep(0.3)
                 # self.data = None
                 #print(self.data_stream)
-            # return self.data
 
-    def get_latlon(self, data):
-        ''' Check and get latitude and longitude '''
-        pass
 
 
 if __name__ == '__main__':
@@ -40,5 +44,8 @@ if __name__ == '__main__':
         while True:
             # gps_data = gps.get_new_data()
             gps.get_new_data()
+            if self.timestamp != None and self.timestamp != "n/a":
+                print(self.location_info)
+                self.timestamp = self.location_info["time"]
     except KeyboardInterrupt:
         gps.shutdown()
