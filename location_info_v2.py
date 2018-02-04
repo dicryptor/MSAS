119e9a74f131586c2ef8d4fc8373f7c5fc651ce8
Mohamed Mubaraq <dicryptor@gmail.com>
2018-02-04 11:16:19 +0800

    New threaded gps information stream

--- location_info_v2.py
+++ location_info_v2.py
@@ -6,25 +6,35 @@
     from the gpsd is then exposed as attributes of that 'data_stream'
 """
 from time import sleep
-
 from gps3.agps3threaded import AGPS3mechanism
 
-__author__ = 'Moe'
-__copyright__ = 'Copyright 2016  Moe'
-__license__ = 'MIT'
-__version__ = '0.2'
+class GPS3():
+    ''' gps3 asynchronous communication module '''
 
-agps_thread = AGPS3mechanism()  # Instantiate AGPS3 Mechanisms
-agps_thread.stream_data()  # From localhost (), or other hosts, by example, (host='gps.ddns.net')
-agps_thread.run_thread()  # Throttle time to sleep after an empty lookup, default 0.2 second, default daemon=True
+    def __init__(self):
+        agps_thread = AGPS3mechanism()  # Instantiate AGPS3 Mechanisms
+        agps_thread.stream_data()  # From localhost (), or other hosts, by example, (host='gps.ddns.net')
+        agps_thread.run_thread()  # Throttle time to sleep after an empty lookup, default 0.2 second, default daemon=True
 
-while True:  # All data is available via instantiated thread data stream attribute.
-    # line #140-ff of /usr/local/lib/python3.5/dist-packages/gps3/agps.py
-    print('---------------------')
-    print(                   agps_thread.data_stream.time)
-    print('Lat:{}   '.format(agps_thread.data_stream.lat))
-    print('Lon:{}   '.format(agps_thread.data_stream.lon))
-    print('Speed:{} '.format(agps_thread.data_stream.speed))
-    print('Course:{}'.format(agps_thread.data_stream.track))
-    print('---------------------')
-    sleep(2)  # Sleep, or do other things for as long as you like.
+
+    def getlatlon(self):
+        return agps_thread.data_stream.lat, agps_thread.data_stream.lon
+
+    def gettime(self):
+        return agps_thread.data_stream.time
+
+
+if __name__ == "__main__":
+    gps3 = GPS3()
+
+    while True:  # All data is available via instantiated thread data stream attribute.
+        print(gps3.getlatlon())
+        # # line #140-ff of /usr/local/lib/python3.5/dist-packages/gps3/agps.py
+        # print('---------------------')
+        # print(                   agps_thread.data_stream.time)
+        # print('Lat:{}   '.format(agps_thread.data_stream.lat))
+        # print('Lon:{}   '.format(agps_thread.data_stream.lon))
+        # print('Speed:{} '.format(agps_thread.data_stream.speed))
+        # print('Course:{}'.format(agps_thread.data_stream.track))
+        # print('---------------------')
+        sleep(2)  # Sleep, or do other things for as long as you like.
