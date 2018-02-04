439d24adf5e1234afb0ec8abb596ed355921b369
Mohamed Mubaraq <dicryptor@gmail.com>
2018-02-04 09:47:06 +0800

    Debugging

--- location_info_v2.py
+++ location_info_v2.py
@@ -1,51 +1,30 @@
-from gps3 import agps3
-import time
+"""TriviaL example using the thread triumvirate
+        agps_thread = AGPS3mechanism()
+        agps_thread.stream_data()
+        agps_thread.run_thread()
+    imported from the agps3threaded.py class AGPS3mechanism.  The unordered associative array
+    from the gpsd is then exposed as attributes of that 'data_stream'
+"""
+from time import sleep
 
-class AGPS():
-    ''' Module for GPS communication using gpsd '''
+from gps3.agps3threaded import AGPS3mechanism
 
-    def __init__(self):
-        self.gpsd_socket = agps3.GPSDSocket()
-        self.gpsd_socket.connect()
-        self.gpsd_socket.watch()
-        self.data_stream = agps3.DataStream()
-        self.location_info = {"time": None, "lat": None, "lon": None, "speed": None, "track": None}
-        self.timestamp = None
+__author__ = 'Moe'
+__copyright__ = 'Copyright 2016  Moe'
+__license__ = 'MIT'
+__version__ = '0.2'
 
+agps_thread = AGPS3mechanism()  # Instantiate AGPS3 Mechanisms
+agps_thread.stream_data()  # From localhost (), or other hosts, by example, (host='gps.ddns.net')
+agps_thread.run_thread()  # Throttle time to sleep after an empty lookup, default 0.2 second, default daemon=True
 
-    def shutdown(self):
-        self.gpsd_socket.close()
-
-
-    def get_new_data(self):
-        ''' check for new data in data stream '''
-        for new_data in self.gpsd_socket:
-            if new_data:
-                self.data_stream.unpack(new_data)
-                if self.timestamp == None: self.timestamp = self.data_stream.time
-                if self.data_stream.lat != "n/a" and self.data_stream.lon != "n/a":
-                    self.location_info["lat"] = self.data_stream.lat
-                    self.location_info["lon"] = self.data_stream.lon
-                    self.location_info["time"] = self.data_stream.time
-                if self.data_stream.speed != "n/a": self.location_info["speed"] = self.data_stream.speed
-                if self.data_stream.track != "n/a": self.location_info["track"] = self.data_stream.track
-                return
-            else:
-                time.sleep(0.3)
-                # self.data = None
-                #print(self.data_stream)
-
-
-
-if __name__ == '__main__':
-    gps = AGPS()
-
-    try:
-        while True:
-            # gps_data = gps.get_new_data()
-            gps.get_new_data()
-            if gps.timestamp != None and gps.timestamp != "n/a":
-                print(gps.location_info)
-                gps.timestamp = gps.location_info["time"]
-    except KeyboardInterrupt:
-        gps.shutdown()
+while True:  # All data is available via instantiated thread data stream attribute.
+    # line #140-ff of /usr/local/lib/python3.5/dist-packages/gps3/agps.py
+    print('---------------------')
+    print(                   agps_thread.data_stream.time)
+    print('Lat:{}   '.format(agps_thread.data_stream.lat))
+    print('Lon:{}   '.format(agps_thread.data_stream.lon))
+    print('Speed:{} '.format(agps_thread.data_stream.speed))
+    print('Course:{}'.format(agps_thread.data_stream.track))
+    print('---------------------')
+    sleep(2)  # Sleep, or do other things for as long as you like.
